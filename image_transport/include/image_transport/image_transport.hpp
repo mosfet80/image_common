97c03dceb25307bbab95975239f9426eff830fc5
Alejandro Hernández Cordero <ahcorde@gmail.com>
2025-08-21 19:34:00 +0200

    Support lifecycle node - NodeInterfaces (#352)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    Co-authored-by: Bence Magyar <bence.magyar.robotics@gmail.com>
    Co-authored-by: Tom Moore <tmoore@locusrobotics.com>

--- image_transport/include/image_transport/image_transport.hpp
+++ image_transport/include/image_transport/image_transport.hpp
@@ -38,6 +38,7 @@
 
 #include "image_transport/camera_publisher.hpp"
 #include "image_transport/camera_subscriber.hpp"
+#include "image_transport/node_interfaces.hpp"
 #include "image_transport/publisher.hpp"
 #include "image_transport/subscriber.hpp"
 #include "image_transport/transport_hints.hpp"
@@ -49,20 +50,18 @@ namespace image_transport
 /*!
  * \brief Advertise an image topic, free function version.
  */
-[[deprecated("Use create_publisher(..., rclcpp::QoS, ...) instead")]]
 IMAGE_TRANSPORT_PUBLIC
+[[deprecated("Use create_publisher(RequiredInterfaces node_interfaces, ..., rclcpp::QoS) "
+  "instead.")]]
 Publisher create_publisher(
   rclcpp::Node * node,
   const std::string & base_topic,
   rmw_qos_profile_t custom_qos = rmw_qos_profile_default,
   rclcpp::PublisherOptions options = rclcpp::PublisherOptions());
 
-/*!
- * \brief Advertise an image topic, free function version.
- */
 IMAGE_TRANSPORT_PUBLIC
 Publisher create_publisher(
-  rclcpp::Node * node,
+  RequiredInterfaces node_interfaces,
   const std::string & base_topic,
   rclcpp::QoS custom_qos,
   rclcpp::PublisherOptions options = rclcpp::PublisherOptions());
@@ -70,7 +69,8 @@ Publisher create_publisher(
 /**
  * \brief Subscribe to an image topic, free function version.
  */
-[[deprecated("Use create_subscription(..., rclcpp::QoS, ...) instead")]]
+[[deprecated("Use create_subscription(RequiredInterfaces node_interfaces, ..., rclcpp::QoS, ...) "
+  "instead")]]
 IMAGE_TRANSPORT_PUBLIC
 Subscriber create_subscription(
   rclcpp::Node * node,
@@ -85,7 +85,7 @@ Subscriber create_subscription(
  */
 IMAGE_TRANSPORT_PUBLIC
 Subscriber create_subscription(
-  rclcpp::Node * node,
+  RequiredInterfaces node_interfaces,
   const std::string & base_topic,
   const Subscriber::Callback & callback,
   const std::string & transport,
@@ -95,7 +95,8 @@ Subscriber create_subscription(
 /*!
  * \brief Advertise a camera, free function version.
  */
-[[deprecated("Use create_camera_publisher(..., rclcpp::QoS, ...) instead")]]
+[[deprecated("Use create_camera_publisher(RequiredInterfaces node_interfaces, ..., "
+  "rclcpp::QoS, ...) instead")]]
 IMAGE_TRANSPORT_PUBLIC
 CameraPublisher create_camera_publisher(
   rclcpp::Node * node,
@@ -108,7 +109,7 @@ CameraPublisher create_camera_publisher(
  */
 IMAGE_TRANSPORT_PUBLIC
 CameraPublisher create_camera_publisher(
-  rclcpp::Node * node,
+  RequiredInterfaces node_interfaces,
   const std::string & base_topic,
   rclcpp::QoS custom_qos,
   rclcpp::PublisherOptions pub_options = rclcpp::PublisherOptions());
@@ -116,7 +117,8 @@ CameraPublisher create_camera_publisher(
 /*!
  * \brief Subscribe to a camera, free function version.
  */
-[[deprecated("Use create_camera_subscription(..., rclcpp::QoS, ...) instead")]]
+[[deprecated("Use create_camera_subscription(RequiredInterfaces node_interfaces, ..., "
+  "rclcpp::QoS, ...) instead")]]
 IMAGE_TRANSPORT_PUBLIC
 CameraSubscriber create_camera_subscription(
   rclcpp::Node * node,
@@ -130,7 +132,7 @@ CameraSubscriber create_camera_subscription(
  */
 IMAGE_TRANSPORT_PUBLIC
 CameraSubscriber create_camera_subscription(
-  rclcpp::Node * node,
+  RequiredInterfaces node_interfaces,
   const std::string & base_topic,
   const CameraSubscriber::Callback & callback,
   const std::string & transport,
@@ -157,7 +159,11 @@ public:
   using CameraInfoConstPtr = sensor_msgs::msg::CameraInfo::ConstSharedPtr;
 
   IMAGE_TRANSPORT_PUBLIC
+  [[deprecated("Use ImageTransport(RequiredInterfaces node_interfaces, ...) instead.")]]
   explicit ImageTransport(rclcpp::Node::SharedPtr node);
+
+  IMAGE_TRANSPORT_PUBLIC
+  explicit ImageTransport(RequiredInterfaces node_interfaces);
 
   IMAGE_TRANSPORT_PUBLIC
   ImageTransport(const ImageTransport & other);
@@ -493,7 +499,7 @@ private:
 
 struct ImageTransport::Impl
 {
-  rclcpp::Node::SharedPtr node_;
+  RequiredInterfaces required_interfaces_;
 };
 
 }  // namespace image_transport
